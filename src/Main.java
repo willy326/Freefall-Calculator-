2b48991ef41c1aa7910f86d822c5c5ee83b8a5ef
willy326 <taeyounghwang9@gmail.com>
2025-05-26 09:02:45 +0900

    Initial Calculations
    Returns initial times based on gravity for each planet.

---
+++ src/Main.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args){
+        
+        Scanner input = new Scanner(System.in);
+
+        System.out.println();
+        System.out.println("----------------------------------------------------------");
+        System.out.println("What height, in meters, are you dropping the ball from?");
+        double height = input.nextDouble();
+        input.nextLine();
+
+        System.out.println("Which planet do you want to drop the ball from? \n Choose from: Mercury, Venus, Earth, Mars, Jupiter, Saturn, Uranus, and Neptune");
+        String planet = input.nextLine();
+
+        System.out.println("What is the mass of the object in kg?");
+        double mass = input.nextDouble();
+
+        Calculations physics = new Calculations();
+        double acceleration = physics.Gravity(planet);
+        
+        System.out.println("----------------------------------------------------------");
+        System.out.println("gravitational acceleration: " + acceleration);
+        
+        double time = physics.fallTime(height,acceleration);
+        System.out.println("Fall time without air resistance: " + time);
+
+        double k = 0.5;   
+        double timeAir = physics.fallTimeWithAirResistance(height, mass, k, acceleration);
+        System.out.println("Fall time with air resistance: " + timeAir);
+        System.out.println("----------------------------------------------------------");
+        System.out.println();
+    }
+}
